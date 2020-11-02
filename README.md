d571d3b9cf4522b23916cacf7bf08a8bc4d7180a
moiseevdanya <moiseev_da@inbox.ru>
2020-11-02 11:54:39 +0300

    update npm

---
+++ README.md
@@ -0,0 +1,7 @@
+# todolist_vue
+
+npm i
+
+npm run serve
+
+npm run build
---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h2>Home page</h2>
+
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quae blanditiis
+      vitae iure repudiandae fugit obcaecati libero sit placeat expedita ullam
+      eius consequuntur quo dolorum, nihil quidem! Blanditiis ipsam id
+      voluptates.
+    </p>
+
+    <router-link to="/todos">Todos</router-link>
+  </div>
+</template>
